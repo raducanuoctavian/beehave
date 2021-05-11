b602d8ffc91fbc65c4bbd259d0afe823daf66efd
raducanuoctavian <82752011+raducanuoctavian@users.noreply.github.com>
2021-05-11 17:44:06 +0300

    commit

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,8 @@
 window.onscroll = function() {onScroll()};
 function onScroll() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("navigation").style.backgroundColor = "grey";
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    document.getElementById("navigation").style.backgroundColor = "#939b62";
   } else {
-    document.getElementById("navigation").style.backgroundColor = "transparent";
+    document.getElementById("navigation").style.backgroundColor = "";
   }
 }
