f6a2606d86763a9324d5798f97c6ad81243d8f68
raducanuoctavian <82752011+raducanuoctavian@users.noreply.github.com>
2021-07-18 13:44:53 +0300

    Blog inceput + modificari html + teste

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,16 @@
 window.onscroll = function() {onScroll()};
 function onScroll() {
   if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    document.getElementById("navigation").style.backgroundColor = "#939b62";
+    document.getElementById("navigation").style.backgroundColor = "white";
+	var elements = document.getElementsByClassName("nav-link");
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.color = "black";
+	}
+
   } else {
     document.getElementById("navigation").style.backgroundColor = "";
+	var elements = document.getElementsByClassName("nav-link");
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.color = "";
   }
-}
+}}
